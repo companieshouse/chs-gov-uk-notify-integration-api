53393571fdcbd4e930179cc65832fc48db38f442
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-02-17 13:32:04 +0000

    removed unused files

--- src/test/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/common/Preprocessors/Preprocessor.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.common.Preprocessors;
-
-public abstract class Preprocessor {
-
-    public abstract Object preprocess(final Object object);
-
-}
