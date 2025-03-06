683e56e0996f3336d3a8df01d74977f22f515ef4
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-03-06 07:35:35 +0000

    move templates and add package structure

---
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/govukconnection/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * handles the connection to the external Gov.uk Notify service
+ */
+package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.govukconnection;
---
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/TemplateLookupInterface.java
@@ -0,0 +1,6 @@
+package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.templatelookup;
+
+public interface TemplateLookupInterface {
+
+    ChTemplate retrieveTemplate(String templateId);
+}
