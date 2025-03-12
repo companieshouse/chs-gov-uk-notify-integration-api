30b82f51c2a81c882c72633a946c549c7467089f
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-03-12 07:31:55 +0000

    add more interfaces

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/emailgovuknotifypayload/EmailGovUkNotifyPayloadInterface.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/emailgovuknotifypayload/EmailGovUkNotifyPayloadInterface.java
@@ -1,7 +1,15 @@
 package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.emailgovuknotifypayload;
 
 import org.springframework.stereotype.Component;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.govukconnection.GovUkConnectionInterface;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.templatelookup.TemplateLookupInterface;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.templatepersonalisation.TemplatePersonalisationInterface;
 
 @Component
 public interface EmailGovUkNotifyPayloadInterface {
+
+    TemplateLookupInterface templateLookup = null;
+    TemplatePersonalisationInterface templatePersonalisation = null;
+    GovUkConnectionInterface govUkConnection = null;
+
 }
--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/restapi/RestApi.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/restapi/RestApi.java
@@ -1,23 +1,41 @@
 package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.restapi;
 
 import jakarta.validation.Valid;
+import org.apache.commons.lang.NotImplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import uk.gov.companieshouse.api.chs_gov_uk_notify_integration_api.api.NotificationSenderInterface;
 import uk.gov.companieshouse.api.chs_gov_uk_notify_integration_api.model.GovUkEmailDetailsRequest;
 import uk.gov.companieshouse.api.chs_gov_uk_notify_integration_api.model.GovUkLetterDetailsRequest;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.emailgovuknotifypayload.EmailGovUkNotifyPayloadInterface;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.lettergovuknotifypayload.LetterGovUkNotifyPayloadInterface;
 
 @Controller
 @Validated
 public class RestApi implements NotificationSenderInterface {
-    @Override
-    public ResponseEntity<Void> sendEmail(@Valid GovUkEmailDetailsRequest govUkEmailDetailsRequest, String s) {
-        return null;
+
+    EmailGovUkNotifyPayloadInterface emailGovUkNotifyPayload;
+    LetterGovUkNotifyPayloadInterface letterGovUkNotifyPayload;
+
+    public RestApi(EmailGovUkNotifyPayloadInterface emailGovUkNotifyPayload, LetterGovUkNotifyPayloadInterface letterGovUkNotifyPayload) {
+        this.emailGovUkNotifyPayload = emailGovUkNotifyPayload;
+        this.letterGovUkNotifyPayload = letterGovUkNotifyPayload;
     }
 
     @Override
-    public ResponseEntity<Void> sendLetter(@Valid GovUkLetterDetailsRequest govUkLetterDetailsRequest, String s) {
-        return null;
+    public ResponseEntity<Void> sendEmail(@Valid GovUkEmailDetailsRequest govUkEmailDetailsRequest, String xHeaderId) {
+
+        //FIXME :  call emailGovUkNotifyPayload
+
+        throw new NotImplementedException();
+    }
+
+    @Override
+    public ResponseEntity<Void> sendLetter(@Valid GovUkLetterDetailsRequest govUkLetterDetailsRequest, String xHeaderId) {
+
+        //FIXME :  call letterGovUkNotifyPayload
+
+        throw new NotImplementedException();
     }
 }
