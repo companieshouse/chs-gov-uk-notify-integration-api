71628b5bb732dfbbb3cbb00694f2da84acac4683
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-03-24 16:25:19 +0000

    add thymeleaf dependency

---
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/lettergovuknotifypayload/LetterGovUkNotifyPayloadImpl.java
@@ -0,0 +1,7 @@
+package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.lettergovuknotifypayload;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LetterGovUkNotifyPayloadImpl implements LetterGovUkNotifyPayloadInterface {
+}
