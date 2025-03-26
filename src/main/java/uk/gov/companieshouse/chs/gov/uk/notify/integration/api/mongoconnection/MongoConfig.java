8d54123306b986fd780a0e1957901c31280b7c95
aaspellclark-ch <aaspellclark@companieshouse.gov.uk>
2025-03-26 16:54:26 +0000

    update config

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/mongoconnection/MongoConfig.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/mongoconnection/MongoConfig.java
@@ -1,5 +1,11 @@
 package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongoconnection;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
@@ -12,12 +18,27 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.Optional;
 
 @Configuration
 @EnableMongoRepositories("uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongoconnection")
 @EnableMongoAuditing(dateTimeProviderRef = "mongodbDatetimeProvider")
 class MongoConfig {
+
+    private String mongoDbHost;
+    private String mongoDbPort;
+    private String mongoDbUserName;
+    private String mongoDbPassword;
+    private String mongoDbDatabase;
+
+    public MongoConfig(@Value("spring.data.mongodb.host") String mongoDbHost, @Value("spring.data.mongodb.port") String mongoDbPort, @Value("spring.data.mongodb.username") String mongoDbUserName, @Value("spring.data.mongodb.password") String mongoDbPassword, @Value("spring.data.mongodb.database") String mongoDbDatabase) {
+        this.mongoDbHost = mongoDbHost;
+        this.mongoDbPort = mongoDbPort;
+        this.mongoDbUserName = mongoDbUserName;
+        this.mongoDbPassword = mongoDbPassword;
+        this.mongoDbDatabase = mongoDbDatabase;
+    }
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener(final LocalValidatorFactoryBean factory) {
@@ -34,9 +55,18 @@ class MongoConfig {
         return new MongoTemplate(mongoDatabaseFactory());
     }
 
+    @Bean()
+    public MongoClient mongoClient() {
+        MongoCredential credential = MongoCredential.createCredential(mongoDbUserName, mongoDbDatabase, mongoDbPassword.toCharArray());
+
+        return MongoClients.create(MongoClientSettings.builder()
+                                                      .applyToClusterSettings(builder -> builder.hosts(Collections.singletonList(new ServerAddress(mongoDbHost + ":" + mongoDbPort))))
+                                                      .credential(credential).build());
+    }
+
     @Bean
     MongoDatabaseFactory mongoDatabaseFactory() {
-        return new SimpleMongoClientDatabaseFactory("mongodb://localhost:27017/account");
+        return new SimpleMongoClientDatabaseFactory("mongodb://" + mongoDbHost + ":" + mongoDbPort + "/account");
     }
 
 }
