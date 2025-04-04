75d81f3d44b0f040878a785adfde9a869100806e
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-04-04 16:30:00 +0100

    DEEP-230: Improve context ID validation error message.

--- src/test/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/restapi/SenderRestApiIntegrationTest.java
+++ src/test/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/restapi/SenderRestApiIntegrationTest.java
@@ -20,6 +20,7 @@ import static org.apache.commons.io.IOUtils.resourceToString;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -29,6 +30,11 @@ class SenderRestApiIntegrationTest {
 
     private static final String CONTEXT_ID = "X9uND6rXQxfbZNcMVFA7JI4h2KOh";
     private static final String INVALID_CONTEXT_ID = "X9uND6rXQxfbZ:cMVFA7JI4h2KOh";
+    private static final String INVALID_CONTEXT_ID_ERROR_MESSAGE_PREFIX =
+            "Error in chs-gov-uk-notify-integration-api: context ID (X-Request-ID): must match ";
+    private static final String CONTEXT_ID_PATTERN = "[0-9A-Za-z-_]{8,32}";
+    private static final String INVALID_CONTEXT_ID_ERROR_MESSAGE =
+            INVALID_CONTEXT_ID_ERROR_MESSAGE_PREFIX + "\"" + CONTEXT_ID_PATTERN + "\"";
 
     @Autowired
     private MockMvc mockMvc;
@@ -60,7 +66,7 @@ class SenderRestApiIntegrationTest {
 
     @Test
     @DisplayName("Send letter with an invalid context ID")
-    void sendLetterWithInvalidContextId() throws Exception {
+    void sendLetterWithInvalidContextId(CapturedOutput log) throws Exception {
 
         // When and then
         mockMvc.perform(post("/gov-uk-notify-integration/letter")
@@ -68,8 +74,11 @@ class SenderRestApiIntegrationTest {
                         .accept(MediaType.APPLICATION_JSON)
                         .header("X-Request-ID", INVALID_CONTEXT_ID)
                         .content(resourceToString("/fixtures/send-letter-request.json", UTF_8)))
-                .andExpect(status().isBadRequest());
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(INVALID_CONTEXT_ID_ERROR_MESSAGE));
 
+        assertThat(log.getAll().contains(INVALID_CONTEXT_ID_ERROR_MESSAGE_PREFIX), is(true));
+        assertThat(log.getAll().contains(CONTEXT_ID_PATTERN), is(true));
     }
 
 }
