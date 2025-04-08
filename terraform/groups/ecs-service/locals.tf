da424532f2e704c52850b1e85b7dace6a1c45ec3
Jbowman-CH <jbowman@companieshouse.gov.uk>
2025-04-08 11:11:32 +0100

    Update

--- terraform/groups/ecs-service/locals.tf
+++ terraform/groups/ecs-service/locals.tf
@@ -8,7 +8,7 @@ locals {
   docker_repo                = "chs-gov-uk-notify-integration-api"
   lb_listener_rule_priority  = 18
   lb_listener_paths          = ["/gov-uk-notify-integration/letter", "/gov-uk-notify-integration/email", "/gov-uk-notify-integration/healthcheck"]
-  healthcheck_path           = "/chs-gov-uk-notify-integration-api/healthcheck" #healthcheck path for chs-gov-uk-notify-integration-api service
+  healthcheck_path           = "/gov-uk-notify-integration/healthcheck" #healthcheck path for chs-gov-uk-notify-integration-api service
   healthcheck_matcher        = "200"
   application_subnet_ids     = data.aws_subnets.application.ids
   kms_alias                  = "alias/${var.aws_profile}/environment-services-kms"
