4ce56e1bb58ac6454a3eb1cd2e15e6c212b23378
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-05-13 11:30:44 +0100

    DEEP-287: Rationalise handling of validation exceptions.

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/exception/LetterValidationException.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/exception/LetterValidationException.java
@@ -1,10 +1,12 @@
 package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.exception;
 
+import jakarta.validation.ValidationException;
+
 /**
  * Exception thrown to indicate a problem with the information received in the
  * incoming letter sending request.
  */
-public class LetterValidationException extends RuntimeException {
+public class LetterValidationException extends ValidationException {
     public LetterValidationException(String message) {
         super(message);
     }
--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/restapi/GlobalExceptionHandler.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/restapi/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.restapi;
 
 import static uk.gov.companieshouse.chs.gov.uk.notify.integration.api.ChsGovUkNotifyIntegrationService.APPLICATION_NAMESPACE;
 
-import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.ValidationException;
 import java.util.List;
 import java.util.Map;
 import org.springframework.http.HttpHeaders;
@@ -16,7 +16,6 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.exception.LetterValidationException;
 import uk.gov.companieshouse.logging.Logger;
 import uk.gov.companieshouse.logging.util.DataMap;
 
@@ -43,14 +42,14 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
      * Returns HTTP Status 400 Bad Request when there is an exception implying that
      * the incoming request content is invalid.
      *
-     * @param cve exception thrown when input from request is found to be invalid
+     * @param ve exception thrown when input from request is found to be invalid
      * @return response with payload reporting underlying cause
      */
-    @ExceptionHandler(ConstraintViolationException.class)
-    public ResponseEntity<Object> handleConstraintViolationException(
-            ConstraintViolationException cve) {
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Object> handleValidationException(
+            ValidationException ve) {
         var message = "Error in " + APPLICATION_NAMESPACE + ": "
-                + buildMessage(cve.getMessage());
+                + buildMessage(ve.getMessage());
         myLogger.error("Will handle error `" + message + "` by responding with 400 Bad Request.",
                 getLogMap(message));
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -75,24 +74,6 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         }
 
         return super.handleMethodArgumentNotValid(manve, headers, status, request);
-    }
-
-    /**
-     * Returns HTTP Status 400 Bad Request when there is an exception implying that
-     * the incoming letter request content is invalid.
-     *
-     * @param lve exception thrown when input from letter sending request is found to be invalid
-     * @return response with payload reporting underlying cause
-     */
-    @ExceptionHandler(LetterValidationException.class)
-    public ResponseEntity<Object> handleLetterValidationException(
-            LetterValidationException lve) {
-        var message = "Error in " + APPLICATION_NAMESPACE + ": "
-                + buildMessage(lve.getMessage());
-        myLogger.error("Will handle error `" + message + "` by responding with 400 Bad Request.",
-                getLogMap(message));
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(message);
     }
 
     /**
