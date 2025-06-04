786498fcb9f608883399df2b348f8e828dd04e49
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-06-04 08:46:45 +0100

    DEEP-288: Remove unused code.

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/pdfgenerator/PdfGeneratorInterface.java
+++
@@ -1,7 +0,0 @@
-package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.pdfgenerator;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public interface PdfGeneratorInterface {
-}
