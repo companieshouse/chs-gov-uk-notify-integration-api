533ecf69bef86d6d2509079f4307890afe06e904
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-06-05 11:40:51 +0100

    DEEP-288: Extend EmptyReplacedElement to avoid implementing irrelevant code.

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/pdfgenerator/SvgReplacedElement.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/pdfgenerator/SvgReplacedElement.java
@@ -3,25 +3,20 @@ package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.pdfgenerator;
 import static org.xhtmlrenderer.css.style.CalculatedStyle.BOTTOM;
 import static org.xhtmlrenderer.css.style.CalculatedStyle.LEFT;
 
-import java.awt.Point;
 import java.awt.print.PageFormat;
 import java.awt.print.Paper;
 import org.apache.batik.transcoder.TranscoderInput;
 import org.apache.batik.transcoder.print.PrintTranscoder;
 import org.w3c.dom.Document;
-import org.xhtmlrenderer.layout.LayoutContext;
+import org.xhtmlrenderer.pdf.EmptyReplacedElement;
 import org.xhtmlrenderer.pdf.ITextOutputDevice;
-import org.xhtmlrenderer.pdf.ITextReplacedElement;
 import org.xhtmlrenderer.render.BlockBox;
 import org.xhtmlrenderer.render.PageBox;
 import org.xhtmlrenderer.render.RenderingContext;
 
-public class SvgReplacedElement implements ITextReplacedElement {
+public class SvgReplacedElement extends EmptyReplacedElement {
 
-    private final Point location = new Point(0, 0);
     private final Document svg;
-    private final int cssWidth;
-    private final int cssHeight;
 
     /**
      * Constructor.
@@ -30,53 +25,8 @@ public class SvgReplacedElement implements ITextReplacedElement {
      * @param cssHeight the height of the image
      */
     public SvgReplacedElement(Document svg, int cssWidth, int cssHeight) {
-        this.cssWidth = cssWidth;
-        this.cssHeight = cssHeight;
+        super(cssWidth, cssHeight);
         this.svg = svg;
-    }
-
-    @Override
-    public void detach(LayoutContext layoutContext) {
-        // No dynamic behaviour anticipated for this SVG element. Once it's in the document,
-        // it should stay there. Hence, nothing to implement here.
-    }
-
-    @Override
-    public int getBaseline() {
-        return 0;
-    }
-
-    @Override
-    public int getIntrinsicWidth() {
-        return cssWidth;
-    }
-
-    @Override
-    public int getIntrinsicHeight() {
-        return cssHeight;
-    }
-
-    @Override
-    public boolean hasBaseline() {
-        return false;
-    }
-
-    @Override
-    public boolean isRequiresInteractivePaint() {
-        return false;
-    }
-
-    @Override
-    public Point getLocation() {
-        return location;
-    }
-
-    // x and y are from a universal naming convention
-    @SuppressWarnings("checkstyle:ParameterName")
-    @Override
-    public void setLocation(int x, int y) {
-        this.location.x = x;
-        this.location.y = y;
     }
 
     // x and y are from a universal naming convention
@@ -85,8 +35,8 @@ public class SvgReplacedElement implements ITextReplacedElement {
     public void paint(RenderingContext renderingContext, ITextOutputDevice outputDevice,
                       BlockBox blockBox) {
 
-        float width = cssWidth / outputDevice.getDotsPerPoint();
-        float height = cssHeight / outputDevice.getDotsPerPoint();
+        float width = getIntrinsicWidth() / outputDevice.getDotsPerPoint();
+        float height = getIntrinsicHeight() / outputDevice.getDotsPerPoint();
 
         var prm = new PrintTranscoder();
         var ti = new TranscoderInput(svg);
@@ -115,7 +65,7 @@ public class SvgReplacedElement implements ITextReplacedElement {
                                              BlockBox blockBox) {
         var page = renderingContext.getPage();
         var x = blockBox.getAbsX() + page.getMarginBorderPadding(renderingContext, LEFT);
-        x = (int)(x / outputDevice.getDotsPerPoint());
+        x = (int) (x / outputDevice.getDotsPerPoint());
         return x;
     }
 
@@ -129,9 +79,9 @@ public class SvgReplacedElement implements ITextReplacedElement {
                 ((PageBox) renderingContext.getRootLayer().getPages().getFirst()).getBottom();
         var length = page.getBottom() - page.getTop();
         var bottom = page.getPageNo() == 0 ? page.getBottom() : p0bottom + length;
-        var y = (bottom - (blockBox.getAbsY() + cssHeight)) + page.getMarginBorderPadding(
-                renderingContext, BOTTOM);
-        y = (int)(y / outputDevice.getDotsPerPoint());
+        var y = (bottom - (blockBox.getAbsY() + getIntrinsicHeight()))
+                + page.getMarginBorderPadding(renderingContext, BOTTOM);
+        y = (int) (y / outputDevice.getDotsPerPoint());
         return y;
     }
 }
