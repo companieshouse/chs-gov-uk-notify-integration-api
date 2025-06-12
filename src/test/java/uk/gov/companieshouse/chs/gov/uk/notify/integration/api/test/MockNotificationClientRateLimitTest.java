1e745af768361a6c98d41cfc80ee8482e6fbddc4
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-06-12 09:08:10 +0100

    DEEP-288: Remove the defaulting of the MONGODB_URL value.

--- src/test/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/test/MockNotificationClientRateLimitTest.java
+++ src/test/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/test/MockNotificationClientRateLimitTest.java
@@ -23,7 +23,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+@SpringBootTest(properties = {"spring.data.mongodb.uri=mongodb://token_value"})
 @ActiveProfiles("test")
 class MockNotificationClientRateLimitTest {
 
