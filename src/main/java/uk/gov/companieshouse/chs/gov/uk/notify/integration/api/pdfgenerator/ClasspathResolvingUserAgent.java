89d535b6b130f53ebe3a2daf44f22727e31853ce
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-06-19 12:28:54 +0100

    DEEP-369: Make packaged JIB built container able to access CSS stylesheets.

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/pdfgenerator/ClasspathResolvingUserAgent.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/pdfgenerator/ClasspathResolvingUserAgent.java
@@ -1,5 +1,6 @@
 package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.pdfgenerator;
 
+import java.io.InputStream;
 import java.util.Optional;
 import org.xhtmlrenderer.pdf.ITextOutputDevice;
 import org.xhtmlrenderer.pdf.ITextUserAgent;
@@ -15,5 +16,38 @@ public class ClasspathResolvingUserAgent extends ITextUserAgent {
         // default URI resolution.
         var url = Optional.ofNullable(getClass().getClassLoader().getResource(fileNameOrUri));
         return url.isPresent() ? url.get().getPath() : super.resolveURI(fileNameOrUri);
+    }
+
+    // TODO DEEP-369 Sort this out.
+    @Override
+    protected InputStream resolveAndOpenStream(String uri) {
+
+        InputStream is = null;
+        uri = this.resolveURI(uri);
+
+        uri = stripOffJarContext(uri);
+
+        try {
+            is = this.getClass().getResourceAsStream(uri);
+        }  finally {
+            System.out.println("Resolving " + uri + " to " + is);
+        }
+
+        return is;
+    }
+
+    /**
+     * Takes a URI such as
+     * <code>file:/app/classpath/chs-gov-uk-notify-integration-api-unversioned.original.jar!
+     * /assets/templates/letters/common/notify_letter_layout.css"</code>
+     * and returns the same path without its Jar context,
+     * <code>/assets/templates/letters/common/notify_letter_layout.css</code>.
+     *
+     * @param uri the URI which may include a JAR context
+     * @return the same URI minus any jar context or prefix, leaving only the relative path of the
+     * resource sought within the jar.
+     */
+    private String stripOffJarContext(String uri) {
+        return uri.substring(uri.lastIndexOf('!') + 1);
     }
 }
