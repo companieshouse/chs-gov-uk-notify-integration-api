6256278a03fa30adefdfd5a55531b5dc6515d613
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-07-10 16:23:43 +0100

    DEEP-268: Sonar. Define a constant instead of duplicating literal "chips" 3 times.

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/LetterTemplateKey.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/LetterTemplateKey.java
@@ -12,13 +12,18 @@ import java.math.BigDecimal;
  */
 public record LetterTemplateKey(String appId, String id, BigDecimal version) {
 
+    private static final String CHIPS_APPLICATION_ID = "chips";
+
     public static final LetterTemplateKey CHIPS_DIRECTION_LETTER_1 =
-            new LetterTemplateKey("chips", "direction_letter", ONE);
+            new LetterTemplateKey(CHIPS_APPLICATION_ID, "direction_letter", ONE);
 
     public static final LetterTemplateKey CHIPS_NEW_PSC_DIRECTION_LETTER_1 =
-            new LetterTemplateKey("chips", "new_psc_direction_letter", ONE);
+            new LetterTemplateKey(CHIPS_APPLICATION_ID, "new_psc_direction_letter", ONE);
 
     public static final LetterTemplateKey CHIPS_TRANSITIONAL_NON_DIRECTOR_PSC_INFORMATION_LETTER_1 =
-            new LetterTemplateKey("chips", "transitional_non_director_psc_information_letter", ONE);
+            new LetterTemplateKey(
+                    CHIPS_APPLICATION_ID,
+                    "transitional_non_director_psc_information_letter",
+                    ONE);
 
 }
