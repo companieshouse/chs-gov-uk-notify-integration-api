584ed79e3aca2fcc3ba8f6c2dc2ae7c40be94196
VaughanJackson123 <vjackson1@companieshouse.gov.uk>
2025-09-02 14:20:51 +0100

    DEEP-428: Correct date format to make single digit day of month not start with a leading 0.

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/constants/Constants.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/constants/Constants.java
@@ -6,7 +6,7 @@ public class Constants {
     private Constants() {
     }
 
-    public static final String DATE_FORMAT = "dd MMMM yyyy";
+    public static final String DATE_FORMAT = "d MMMM yyyy";
     public static final DateTimeFormatter DATE_FORMATTER =
             DateTimeFormatter.ofPattern(DATE_FORMAT);
 }
