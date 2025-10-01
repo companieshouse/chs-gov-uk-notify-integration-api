b8d7faa5774bba80f7e64de5e48e59e1a75a8119
Rafa <rgarcia@companieshouse.gov.uk>
2025-10-01 17:14:59 +0100

    Update letter template keys without version

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/LetterTemplateKey.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/LetterTemplateKey.java
@@ -14,21 +14,21 @@ public record LetterTemplateKey(String appId, String id, BigDecimal version) {
 
     public static final String CHIPS_APPLICATION_ID = "chips";
     public static final String DIRECTION_LETTER = "direction_letter";
-    public static final String NEW_PSC_DIRECTION_LETTER = "new_psc_direction_letter";
+    public static final String NEW_PSC_DIRECTION_LETTER = "new_psc_direction_letter_v1";
     public static final String TRANSITIONAL_NON_DIRECTOR_PSC_INFORMATION_LETTER =
-            "transitional_non_director_psc_information_letter";
+            "transitional_non_director_psc_information_letter_v1";
 
     public static final LetterTemplateKey CHIPS_DIRECTION_LETTER_1 =
             new LetterTemplateKey(CHIPS_APPLICATION_ID, DIRECTION_LETTER, ONE);
 
     public static final LetterTemplateKey CHIPS_NEW_PSC_DIRECTION_LETTER_1 =
-            new LetterTemplateKey(CHIPS_APPLICATION_ID, NEW_PSC_DIRECTION_LETTER, ONE);
+            new LetterTemplateKey(CHIPS_APPLICATION_ID, NEW_PSC_DIRECTION_LETTER, null);
 
     public static final LetterTemplateKey CHIPS_TRANSITIONAL_NON_DIRECTOR_PSC_INFORMATION_LETTER_1 =
             new LetterTemplateKey(
                     CHIPS_APPLICATION_ID,
                     TRANSITIONAL_NON_DIRECTOR_PSC_INFORMATION_LETTER,
-                    ONE);
+                    null);
 
     public static final LetterTemplateKey CHIPS_EXTENSION_ACCEPTANCE_LETTER_1 =
             new LetterTemplateKey(CHIPS_APPLICATION_ID, "extension_acceptance_letter", ONE);
