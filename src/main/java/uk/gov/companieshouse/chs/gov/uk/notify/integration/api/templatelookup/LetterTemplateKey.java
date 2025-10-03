b852835232c41557a44dc06776f946ff0581cd68
Rafa <rgarcia@companieshouse.gov.uk>
2025-10-03 14:02:37 +0100

    Remove version from javadoc

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/LetterTemplateKey.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/templatelookup/LetterTemplateKey.java
@@ -4,7 +4,6 @@ package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.templatelookup;
  * Key identifying a letter template.
  * @param appId the ID of the client app (aka service) requesting the sending of the letter
  * @param id the template ID (aka name) corresponding to the type of letter to be sent
- * @param version the version of the template
  */
 public record LetterTemplateKey(String appId, String id) {
 
