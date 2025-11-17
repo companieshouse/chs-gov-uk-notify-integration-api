687cbc83af734cd64768983517422bcd395926e1
kpatel-ch <kpatel@companieshouse.gov.uk>
2025-11-17 10:43:01 +0000

    DEEP-596: Added mongo conversions back

--- src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/config/MongoConfig.java
+++ src/main/java/uk/gov/companieshouse/chs/gov/uk/notify/integration/api/config/MongoConfig.java
@@ -1,16 +1,20 @@
 package uk.gov.companieshouse.chs.gov.uk.notify.integration.api.config;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongo.converter.DateToOffsetDateTimeConverter;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongo.converter.DocumentToLetterResponseConverter;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongo.converter.DocumentToSendEmailResponseConverter;
+import uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongo.converter.OffsetDateTimeToDateConverter;
 
 @Configuration
 @EnableMongoRepositories("uk.gov.companieshouse.chs.gov.uk.notify.integration.api.mongo.repository")
@@ -18,14 +22,19 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 @EnableTransactionManagement
 public class MongoConfig {
 
-    @Bean
-    public ValidatingMongoEventListener validatingMongoEventListener( final LocalValidatorFactoryBean factory ) {
-        return new ValidatingMongoEventListener( factory );
-    }
-
     @Bean( name = "mongodbDatetimeProvider" )
     public DateTimeProvider dateTimeProvider() {
         return () -> Optional.of( LocalDateTime.now() );
+    }
+
+    @Bean
+    public MongoCustomConversions mongoCustomConversions() {
+        return new MongoCustomConversions(List.of(
+                new DateToOffsetDateTimeConverter(),
+                new OffsetDateTimeToDateConverter(),
+                new DocumentToSendEmailResponseConverter(),
+                new DocumentToLetterResponseConverter()
+        ));
     }
 
 }
