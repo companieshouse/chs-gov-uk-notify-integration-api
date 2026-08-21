815a9b29a6d5ce976c9def48588cc7183893dd4a
makhtar-ch <makhtar@companieshouse.gov.uk>
2026-08-21 12:35:33 +0100

    add tagging

--- terraform/groups/ecs-service/iam.tf
+++ terraform/groups/ecs-service/iam.tf
@@ -1,6 +1,13 @@
 resource "aws_iam_role" "notifications_read" {
   name               = "${local.name_prefix}-read"
   assume_role_policy = data.aws_iam_policy_document.read_trust.json
+
+  tags = {
+    Name               = "${var.environment}-${local.service_name}-task-role"
+    Environment        = var.environment
+    ECSClusterName     = "${local.name_prefix}-cluster"
+    ManagedByTerraform = "true"
+  }
 }
 
 resource "aws_iam_role_policy" "read_from_s3" {
